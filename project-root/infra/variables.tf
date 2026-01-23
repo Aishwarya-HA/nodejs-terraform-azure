ea9eacd0fb478a4b0612ce90463e43fcd446402c
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-01-23 14:18:56 +0530

    Create variables.tf

---
+++ project-root/infra/variables.tf
@@ -0,0 +1,5 @@
+
+variable "location"          { default = "west europe" }
+variable "resource_group"    { default = "rg-nodejs-demo" }
+variable "app_service_plan"  { default = "asp-nodejs-demo" }
+variable "webapp_name"       { default = "nodejs-webapp-demo-001" }
