96c4435025abdb038b67229e9f965ccc9811bc8c
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-01-26 22:58:28 +0530

    Update main.tf

--- project-root/infra/main.tf
+++ project-root/infra/main.tf
@@ -25,10 +25,10 @@ resource "azurerm_service_plan" "plan" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "B1" # Use S1/P1v3 for production
 }
 
-# Linux Web App (native Node.js)
+# Linux Web App (Node 20 LTS)
 resource "azurerm_linux_web_app" "app" {
   name                = var.webapp_name
   location            = azurerm_resource_group.rg.location
@@ -38,12 +38,13 @@ resource "azurerm_linux_web_app" "app" {
 
   site_config {
     application_stack {
-      node_version = "22-lts"
+      node_version = "20-lts"
     }
   }
 
   app_settings = {
-    SCM_DO_BUILD_DURING_DEPLOYMENT = "true"
-    WEBSITE_RUN_FROM_PACKAGE       = "1"
+    # Build with Oryx during Zip Deploy
+    "SCM_DO_BUILD_DURING_DEPLOYMENT" = "true"
+    "WEBSITE_RUN_FROM_PACKAGE"       = "1"
   }
 }
