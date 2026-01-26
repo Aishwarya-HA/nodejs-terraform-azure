4a36a5b73ad9ed76a7814191ff6e4b82b85730ad
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-01-26 23:05:30 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const app = express();
 
-// App Service provides the listening port via process.env.PORT
 const port = process.env.PORT || 8080;
 
 app.get('/healthz', (_req, res) => res.status(200).send('OK'));
