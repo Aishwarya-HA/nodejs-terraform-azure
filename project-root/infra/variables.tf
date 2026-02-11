777a7d63a5f91b489ebf4f819a174c168542ad4f
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-02-11 15:37:34 +0530

    Update variables.tf

--- project-root/infra/variables.tf
+++ project-root/infra/variables.tf
@@ -1,5 +1,23 @@
+variable "location" {
+  description = "Azure region for all resources"
+  type        = string
+  default     = "Central India"
+}
 
-variable "location"         { type = string  default = "Central India" }
-variable "resource_group"   { type = string  default = "rg-nodejs-app" }
-variable "app_service_plan" { type = string  default = "appnodejslinux" }
-variable "webapp_name"      { type = string  default = "nodejs-app-aishwarya" }
+variable "resource_group" {
+  description = "Resource group name"
+  type        = string
+  default     = "rg-nodejs-app"
+}
+
+variable "app_service_plan" {
+  description = "Linux App Service Plan name"
+  type        = string
+  default     = "asp-nodejs-linux"
+}
+
+variable "webapp_name" {
+  description = "Linux Web App name (must be globally unique)"
+  type        = string
+  default     = "nodejs-app-aishwarya-tf"  
+}
